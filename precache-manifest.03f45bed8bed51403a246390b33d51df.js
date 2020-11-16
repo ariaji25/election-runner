5cb96690d4d58b68f7e3c6cbc8eb8a0f6836d7f0
Ari Purnama <ari.purnama@citra.digital>
2020-11-16 09:41:12 +0700

    init

---
+++ precache-manifest.03f45bed8bed51403a246390b33d51df.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b03b0b1a5dcb71189a8c7f5626d9cd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3c5c910eb8dac68f9104",
+    "url": "/static/css/2.7a917bdd.chunk.css"
+  },
+  {
+    "revision": "63eca74efb903028643e",
+    "url": "/static/css/main.e22e5544.chunk.css"
+  },
+  {
+    "revision": "3c5c910eb8dac68f9104",
+    "url": "/static/js/2.2814a5f7.chunk.js"
+  },
+  {
+    "revision": "edf34e01a81ebf488b26022bfb2cf750",
+    "url": "/static/js/2.2814a5f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63eca74efb903028643e",
+    "url": "/static/js/main.1c383661.chunk.js"
+  },
+  {
+    "revision": "5262327ef3dc90fc23f5",
+    "url": "/static/js/runtime-main.6e03aaa3.js"
+  },
+  {
+    "revision": "dea4998b081c6c1133a3b5b08ff2218c",
+    "url": "/static/media/Metropolis-Bold.dea4998b.otf"
+  },
+  {
+    "revision": "f7b5e589f88206b4bd5cb1408c5362e6",
+    "url": "/static/media/Metropolis-Regular.f7b5e589.otf"
+  },
+  {
+    "revision": "6ff66a06a9ab303bf18d500cb69dd9c0",
+    "url": "/static/media/dukungcalonmu_icon.6ff66a06.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ecdcfec47bba4988cf5223288cd50dd3",
+    "url": "/static/media/report.ecdcfec4.svg"
+  }
+]);
